499cbb54dc09073e894df1854b973a6fb0ad9fbe
Siobhan Platten <siobhan.platten@gmail.com>
2023-08-19 08:50:05 +0100

    timestamped builds

--- test_package/src/index.js
+++ test_package/src/index.js
@@ -6,7 +6,6 @@ document.body.style.margin = "0";
 document.body.style.padding = "0";
 
 const title = document.createElement("p");
-title.innerText = "Babylon.js NPM Package Template";
 title.style.fontSize = "32pt";
 title.style.textAlign = "center";
 document.body.appendChild(title);
@@ -26,8 +25,10 @@ div.appendChild(canvas);
 
 let assetsHostUrl;
 if (DEV_BUILD) {
+    title.innerText = "Dev Build";
     assetsHostUrl = "http://127.0.0.1:8181/";
 } else {
+    title.innerText = "Deploy POC";
     assetsHostUrl = "https://nonlocal-assets-host-url/";
 }
 initializeBabylonApp({ canvas: canvas, assetsHostUrl: assetsHostUrl });
