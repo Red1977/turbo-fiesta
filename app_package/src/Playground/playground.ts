325c05c4acaf02be35744edb670f5287b40955d0
Siobhan Platten <siobhan.platten@gmail.com>
2023-08-19 08:00:18 +0100

    deploy build

--- app_package/src/Playground/playground.ts
+++ app_package/src/Playground/playground.ts
@@ -25,7 +25,7 @@ class Playground {
         var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
 
         // Move the sphere upward 1/2 its height
-        sphere.position.y = 3;
+        sphere.position.y = 2;
 
         // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
         var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
